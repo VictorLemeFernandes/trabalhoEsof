2742f94012b3e02a754fa328aa4aae517bc6c51c
Atilio Gabriello <atilioga22@gmail.com>
2023-06-23 19:26:00 -0300

    Pagina home criada

--- api/api/src/main/java/trabalhoScrum/api/controllers/UserController.java
+++ api/api/src/main/java/trabalhoScrum/api/controllers/UserController.java
@@ -2,9 +2,13 @@ package trabalhoScrum.api.controllers;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+
+import java.io.Console;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import trabalhoScrum.api.dto.DadosCadastroUsuario;
+import trabalhoScrum.api.usuario.Cargo;
 import trabalhoScrum.api.usuario.Usuario;
 import trabalhoScrum.api.usuario.UsuarioRepository;
 
@@ -32,6 +36,16 @@ public class UserController {
             }
         } else {
             return -2; // Email inválido
+        }
+    }
+
+    @GetMapping("/verificaCadastro") // FUNCIONANDO CORRETAMENTE(?)
+    public long logar(@RequestParam long id) {
+        var usr = repository.findById(id);
+        if (usr.get().getCargo() == Cargo.PRODUCT_OWNER) {
+            return (long) 1;
+        } else {
+            return (long) 0; // Email inválido
         }
     }
 
--- api/api/src/main/resources/static/home.js
+++ api/api/src/main/resources/static/home.js
@@ -1,6 +1,6 @@
-// window.onload = alert(localStorage.getItem("ID"))
+window.onload = verificaId()
 
-function requisitos() {
+function criarRequisitos() {
     fetch('http://localhost:3000/requisitos')
         .then(response => response.json())
         .then(data => {
@@ -11,11 +11,31 @@ function requisitos() {
         });
 }
 
-function recebeDados(id){
-    document.location.href = "home.html"
-    console.log(id)
+function verificaId() {
+    id = localStorage.getItem("ID")
+    fetch(`http://localhost:8080/users/verificaCadastro?id=${id}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            if (data == 1) {
+                document.getElementById("cadastrar-requisito").removeAttribute("hidden")
+            }
+        })
+        .catch(error => { console.error(error) })
 }
 
-function verificaId(id) {
-    
+const openModalBtn = document.getElementById('openModalBtn');
+const modal = document.getElementById('modal');
+const closeBtn = document.getElementsByClassName('close')[0];
+
+function openModal() {
+  modal.style.display = 'block';
 }
+
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+openModalBtn.addEventListener('click', openModal);
+
+closeBtn.addEventListener('click', closeModal);
